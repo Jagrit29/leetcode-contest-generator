a483c92e09c9434a1131bff5914d64de5dd54b9a
Jagrit Jagrit <jjagrit@cisco.com>
2024-09-03 08:33:12 +0530

    Updates

---
+++ _next/static/wXucMZX_Zyx02dMbWMtCf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-e4afc46bf1fcfd1d.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
